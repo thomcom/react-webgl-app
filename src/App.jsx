d56c9ee6915b8f117696f72b7d2c64e51e18a767
H. Thomson Comer <thomcom@gmail.com>
2022-10-04 15:48:07 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) Mik BRY
+ * mik@miklabs.com
+ *
+ * This source code is licensed under private license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import './App.css';
+import GLView from './GLView';
+
+function App({ scene }) {
+  return (
+    <div className="App">
+      <GLView width={720} height={480} scene={scene} />
+      <div className="App-title">WebGL React App</div>
+    </div>
+  );
+}
+
+App.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  scene: PropTypes.object.isRequired,
+};
+
+export default App;
